eae7895511a31a7fea44c943f32f9384cdffe277
Walney Negreiros Gomes <36850527+walneygomes@users.noreply.github.com>
2019-06-06 10:25:36 -0300

    Add files via upload

---
+++ Conversor.java
@@ -0,0 +1,21 @@
+
+public class Conversor {
+	private int numero;
+
+	public void numeroAdd(int numero) {
+		this.numero = numero;
+	}
+
+	public int decimal() {
+		return 0;
+	}
+
+	public String hexaDecimal() {
+		return "";
+	}
+
+	public String binario() {
+		return "";
+	}
+
+}
